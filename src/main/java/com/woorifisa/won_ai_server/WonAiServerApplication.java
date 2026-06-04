1a62fef88d070ee574295b01c8ab62e28591c565
caminobelllo <alstpgml0812@gmail.com>
2026-06-04 15:18:33 +0900

    Chore: 초기 레포 세팅

---
+++ src/main/java/com/woorifisa/won_ai_server/WonAiServerApplication.java
@@ -0,0 +1,13 @@
+package com.woorifisa.won_ai_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonAiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonAiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woorifisa/won_ai_server/WonAiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woorifisa.won_ai_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonAiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
